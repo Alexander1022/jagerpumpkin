4151ff627869f24946e4fd912e7f55d9efb4b36d
bymihaylov <boyan.mihailov50@gmail.com>
2026-03-14 17:32:01 +0200

    fix: --build

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 npm run --prefix ./client dev -- --host 127.0.0.1 --port 5173 > /dev/null 2>&1 &
 FRONTEND_PID=$!
 
-docker compose -f ./server/docker-compose.yml up
+docker compose -f ./server/docker-compose.yml up --build
 
 kill $FRONTEND_PID
