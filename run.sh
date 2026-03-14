550225a77064b1cfce9bb2336fc183b8057e68cf
Ivailo41 <ivailorg@gmail.com>
2026-03-14 19:51:17 +0200

    Added --build arg to run script

--- run.sh
+++ run.sh
@@ -3,6 +3,15 @@
 npm run --prefix ./client dev -- --host 127.0.0.1 --port 5173 > /dev/null 2>&1 &
 FRONTEND_PID=$!
 
-docker compose -f ./server/docker-compose.yml up --build
+DOCKERFLAG=""
+if [[ $# -eq 1 && $1 = "--build" ]]; then
+	echo "Building docker images"
+	DOCKERFLAG="$1"
+elif [[ $# -ne 0 ]]; then
+	echo "Bad args"; exit 1
+fi
 
+docker compose -f ./server/docker-compose.yml up ${DOCKERFLAG}
+
+docker compose down
 kill $FRONTEND_PID
