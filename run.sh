b1aa62a891fe38c62a60b96a8d53de0cdb0e0ff0
Ivailo41 <ivailorg@gmail.com>
2026-03-16 16:04:08 +0200

    Automatic hostname getting in client container, Edited run script to check and create needed files for app running

--- run.sh
+++ run.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+if [[ ! -d "server/hidden_service" ]]; then
+	echo "Creating hidden service directory..."
+	mkdir -p server/hidden_service
+	chmod 700 server/hidden_service
+fi
+
+if [[ ! -f "server/.env" ]]; then
+	echo "Creating server .env file..."
+	echo "JWT_SECRET=YourJWTSecret" > server/.env
+fi
+
 DOCKERFLAG="--build"
 if [[ $# -eq 1 && $1 = "--no-build" ]]; then
 	echo "Using existing docker images"
